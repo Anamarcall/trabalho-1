528148e4a16595e058f693b1eeeebf3523cb5ff3
Anamarcall <anacsmarcal@outlook.com>
2024-07-11 21:29:59 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+a = input('Digite o valor: ')
+b = input('Digite o valor: ')
+
+print('Resultado da adição:', int(a) + int(b))
+print('Resultado da subtração:', int(a) - int(b))
+print('Resultado da multiplicação:', int(a) * int(b))
+print('Resultado da divisão:', int(a) / int(b))
